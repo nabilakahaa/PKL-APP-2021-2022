4e9745ba805cd6d7ae8335eb7beb0b885076bf97
nabilakahaa <nabila_184@smkassalaambandung.sch.id>
2022-02-10 15:33:56 +0700

    projek

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        $admin = Role::create([
+            'name' => 'admin',
+            'display_name' => 'User Administrator',
+        ]);
+
+        $pengguna = Role::create([
+            'name' => 'pengguna',
+            'display_name' => 'User Biasa',
+        ]);
+        $user = new User();
+        $user->name = 'Nabila khaerunnisa';
+        $user->email = 'admin@gmail.com';
+        $user->password = Hash::make('12345678');
+        $user->save();
+
+        $pengunjung = new user();
+        $pengunjung->name = 'Altaf Fadli';
+        $pengunjung->email = 'pengunjung@gmail.com';
+        $pengunjung->password = Hash::make('12345678');
+        $pengunjung->save();
+
+        // $pengunjung = new user();
+        // $pengunjung->name='Muhammad Fatah';
+        // $pengunjung->email='fatah@gmail.com';
+        // $pengunjung->password = Hash::make('123456789');
+        // $pengunjung->save();
+
+        // $pengunjung = new user();
+        // $pengunjung->name='Sergio Jason';
+        // $pengunjung->email='jason@gmail.com';
+        // $pengunjung->password = Hash::make('12345678');
+        // $pengunjung->save();
+
+        // $user = new User();
+        // $user->name='Siti Maimunah';
+        // $user->email='siti@gmail.com';
+        // $user->password = Hash::make('12345678');
+        // $user->save();
+
+        $user->attachRole($admin);
+        $pengunjung->attachRole($pengguna);
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,24 @@
+@extends('adminlte::page')
+
+@section('tittle','dashboard')
+
+@section('content_header')
+
+Dashboard
+
+@endsection
+
+@section('content')
+
+Welcome to this beautiful admin panel.
+
+
+@endsection
+
+@section('css')
+
+@endsection
+
+@section('js')
+
+@endsection
